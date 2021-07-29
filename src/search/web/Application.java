0638450bc6af5a17999de7792bda31e3429fe9c3
Megala Ravi <megalaravi@Megalas-MacBook-Air.local>
2021-07-29 03:52:05 -0400

    Added Spell correction feature

--- src/search/web/Application.java
+++ src/search/web/Application.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+import spellcorrector.SpellCorrector;
+
 public class Application {
 
 	public static void main(String[] args) throws IOException {
@@ -14,14 +16,15 @@ public class Application {
 			System.out.println("Choose 1 : Search a url");
 			System.out.println("Choose 2 : Delete cache");
 			System.out.println("Choose 3 : Rank the web pages according to the occurance of a word");
-			System.out.println("Choose 4 : Exit from program");
+			System.out.println("Choose 4 : Words Suggestion");
+			System.out.println("Choose 5 : Exit from program");
 
 //		String url = "https://www.shiksha.com/";
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Please enter your choice");
 			int choice = sc.nextInt();
 			sc = new Scanner(System.in);
-			if (choice == 4) {
+			if (choice == 5) {
 				break;
 			}
 
@@ -55,7 +58,20 @@ public class Application {
 				System.out.println("Please enter a word to be searched");
 				FindWord.readAllFiles();
 				break;
-
+				
+			case 4:
+				System.out.println("Please enter a word to search");
+				String sSearch = sc.nextLine();
+				SpellCorrector corrector = new SpellCorrector();
+				
+				corrector.loadSpellCorrector();
+				String suggestion = corrector.findSimilarWord(sSearch);
+				
+				if (suggestion.length() == 0) 
+				    System.out.println("There are no similar words. Please enter the valid word to search");
+				else
+					System.out.println("Suggestion: " + suggestion);
+				
 			default:
 				System.out.println("Please select a valid number");
 				break;
